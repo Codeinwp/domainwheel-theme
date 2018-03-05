350298f99bf1923cc79fa1544656d59824986a99
Andrei Baicus <baicusandrei@me.com>
2018-03-05 15:11:15 +0200

    Fix mobile ui/ux.

--- header.php
+++ header.php
@@ -26,14 +26,14 @@
 		<!-- Image and text -->
 		<div class="navbar">
 			<div class="container">
-				<div class="col-4 col-md-4 col-lg-3">
+				<div class="brand-wrapper">
 					<a class="navbar-brand" href="<?php echo home_url(); ?>">
                         <img src="<?php echo esc_url( get_template_directory_uri() . '/img/logo_dw.jpg' ); ?>" alt="DomainWheel Logo">
 					</a>
 				</div>
-                <div class="col-8 col-md-8 col-lg-9">
+                <div class="nav-wrapper">
                     <nav id="site-navigation" class="main-navigation">
-                        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="anticon anticon-menu-fold"></i></button>
+                        <button class="ant-btn ant-btn-ghost menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="anticon anticon-menu-fold"></i></button>
                         <?php
                         wp_nav_menu( array(
                             'theme_location' => 'primary-menu',
@@ -50,4 +50,4 @@
 	<div id="content" class="site-content">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-sm-10 offset-sm-1 pb-5 mb-3">
+				<div class="col-xs-12 col-sm-12 pb-5 mb-3">
