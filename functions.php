0291b86d1a9ec0925cdd3a5435a56924f85fb5c5
Cosmin Serbanescu <cosmin@themeisle.com>
2019-06-11 16:39:58 +0300

    fix: proper schema.org #13

--- functions.php
+++ functions.php
@@ -162,3 +162,46 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Add proper schema.org
+ */
+function namegenerator_add_schema_json($data){
+   	echo '
+   	<h1>TEST SCHEMA</h1>
+	<script type="application/ld+json">
+	{
+    	"@context": "http://schema.org",
+    	"@type": "Organization",
+    	"name": "VertiStudio",
+    	"url": "http://vertistudio.com/",
+    	"logo":	"http://vertistudio.com/wp-content/uploads/2018/07/VertiStudio_logo1.png",
+    	"vatID": "RO29109287",
+    	"email": "friends@themeisle.com",
+    	"legalName": "Vertigo Studio SRL",
+    	"foundingDate":	"2012",
+    	"foundingLocation": {
+    		"name": "Bucharest"
+    	},
+    	"numberOfEmployees": {
+    		"value": 25
+    	},
+    	"address": {
+    		"streetAddress": "20 Povernei Street, 4th Floor, Flat no. 9",
+    		"addressLocality": "Bucharest",
+    		"addressRegion": "Bucharest",
+    		"postalCode": "010641",
+    		"addressCountry": {
+    			"name": "Romania"
+    		}
+    	},
+    	"founder": {
+    		"name": "Ionut Neagu"
+    	},
+    	"brand": {
+    		"name": "DomainWheel",  
+    		"url": "https://domainwheel.com"
+    	}
+	}
+    </script>';
+}
+add_action( 'wp_footer', 'namegenerator_add_schema_json' );
