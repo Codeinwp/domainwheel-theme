9192d4780c5a518629ff1d29e2ed1bb4ceb7473b
Cosmin Serbanescu <cosmin@themeisle.com>
2019-11-15 16:39:01 +0200

    Neve theme switch #18 WIP

--- functions.php
+++ functions.php
@@ -15,6 +15,9 @@ define( 'DW_VERSION', '2.0.0' );
  * Load styles & scripts
 */
 function neve_child_load_css() {
+	wp_dequeue_style( 'neve-google-font-headings' );
+	wp_dequeue_style( 'neve-google-font-body' );
+	wp_enqueue_style( 'dw-google-font', 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap', array(), false );
 	wp_enqueue_style( 'dw-style', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'neve-style' ), NEVE_VERSION );
 	// wp_dequeue_script( 'jquery' );
 	// wp_deregister_script( 'jquery' );
