8d16b3d2f4ed57079fba7aedf3b8b36ce8d87f94
Cosmin Serbanescu <cosmin@themeisle.com>
2019-11-18 15:41:39 +0200

    neve switch style adjustments #18

--- functions.php
+++ functions.php
@@ -23,3 +23,48 @@ function neve_child_load_css() {
 	// wp_deregister_script( 'jquery' );
 }
 add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
+
+/**
+ * Add proper schema.org
+ */
+function namegenerator_add_schema_json( $data ) {
+	echo '
+	<script type="application/ld+json">
+	{
+    	"@context": "http://schema.org",
+    	"@type": "Organization",
+    	"name": "VertiStudio",
+    	"url": "http://vertistudio.com/",
+    	"logo":	"http://vertistudio.com/wp-content/uploads/2018/07/VertiStudio_logo1.png",
+    	"vatID": "RO29109287",
+    	"email": "friends@themeisle.com",
+    	"legalName": "Vertigo Studio SRL",
+    	"foundingDate":	"2012",
+    	"foundingLocation": {
+    		"name": "Bucharest"
+    	},
+    	"numberOfEmployees": {
+    		"value": 25
+    	},
+    	"address": {
+    		"streetAddress": "20 Povernei Street, 4th Floor, Flat no. 9",
+    		"addressLocality": "Bucharest",
+    		"addressRegion": "Bucharest",
+    		"postalCode": "010641",
+    		"addressCountry": {
+    			"name": "Romania"
+    		}
+    	},
+    	"founder": {
+    		"name": "Ionut Neagu"
+    	},
+    	"brand": {
+    		"@type": "Brand",
+    		"name": "DomainWheel",  
+    		"url": "https://domainwheel.com",
+    		"logo": "' . get_template_directory_uri() . '/img/logo_dw.jpg"
+    	}
+	}
+    </script>';
+}
+add_action( 'wp_footer', 'namegenerator_add_schema_json' );
